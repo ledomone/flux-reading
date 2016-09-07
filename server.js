03d5607d4d27d8c789ec745910c1b24793952bd0
ledomone <ledom.git@gmail.com>
2016-09-07 23:39:24 +0200

    Add morgan logger

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');

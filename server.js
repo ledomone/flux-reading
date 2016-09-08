703f93797ed387d79bc0d7403dd5793058a7195b
ledomone <ledom.git@gmail.com>
2016-09-08 15:06:40 +0200

    Use Swig to initial render first page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var swig  = require('swig');
 
 var app = express();
 
@@ -11,7 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/books.png'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var page = swig.renderFile('views/index.html');
+  res.status(200).send(page);
 });
 
 app.listen(app.get('port'), function () {

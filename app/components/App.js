b623d1c86ec87a9447e3c449854685ff64f59cc7
ledomone <ledom.git@gmail.com>
2016-09-10 11:13:33 +0200

    Add props.children to App component

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div> Hello from React app component </div>
+      <div> Hello from React app component
+        {this.props.children}
+      </div>
     );
   }
 }
